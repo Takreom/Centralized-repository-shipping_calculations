a6701c2aabd6191cde7c6df44bb194d1335aefde
Takreom <abraham.padra.dev@gmail.com>
2026-06-09 22:54:51 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
 # Shipping Cost Calculator
+# Aquí hay una nueva actualización por Takreomgit config --global user.email "your-email@example.com"
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
